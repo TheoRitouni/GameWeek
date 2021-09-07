563141021054dd3e012de07e8b4873d115d44f54
Théo Ritouni <t.ritouni@gmail.com>
2021-09-07 15:27:02 +0200

    spawner player

---
+++ ToutPourLeMaire/Assets/Scripts/Player/SpawnerPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SpawnerPlayer : MonoBehaviour
+{
+    private int countPlayer = 0;
+    [SerializeField]private Transform spawn1;
+    [SerializeField]private Transform spawn2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPlayerJoined(PlayerInput playerInput)
+    { 
+        countPlayer++;
+
+        if (countPlayer == 1)
+            playerInput.gameObject.transform.position = spawn1.position;
+
+        if (countPlayer == 2)
+            playerInput.gameObject.transform.position = spawn2.position;
+
+    }
+
+    private void OnPlayerLeft()
+    {
+        countPlayer--;
+    }
+}
