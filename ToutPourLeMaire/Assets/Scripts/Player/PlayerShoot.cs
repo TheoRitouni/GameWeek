87566f08f7e4b914058c403c0d307086a5e79096
Rémy Ginoyer <ginoyerr@gmail.com>
2021-09-07 15:38:36 +0200

    Tweak Screenshake + Fix Player Spawn + Add meshes on LD

--- ToutPourLeMaire/Assets/Scripts/Player/PlayerShoot.cs
+++ ToutPourLeMaire/Assets/Scripts/Player/PlayerShoot.cs
@@ -1,10 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
     [SerializeField] private GameObject flyerBullet;
+    [SerializeField] private GameObject flyerSpawner;
+
+    public Action onShoot;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +24,7 @@ public class PlayerShoot : MonoBehaviour
 
     void OnShoot()
     {
-        Instantiate(flyerBullet, gameObject.transform.position , transform.rotation);
+        Instantiate(flyerBullet, flyerSpawner.transform.position , transform.rotation);
+        onShoot?.Invoke();
     }
 }
