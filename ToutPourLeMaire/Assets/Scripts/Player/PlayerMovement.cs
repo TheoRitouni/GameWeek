6daa469f193fa370ee6bc36c55475fff489e8cc8
Théo Ritouni <t.ritouni@gmail.com>
2021-09-07 13:02:34 +0200

    Player basic movement

---
+++ ToutPourLeMaire/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector2 movement;
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float rotationSpeed = 20f;
+
+    private Rigidbody rigid;
+    private Vector3 frameMovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void OnMove(InputValue value)   
+    {
+        movement = value.Get<Vector2>();
+
+        if (movement.sqrMagnitude > 0.1)
+        {
+            float angle = Mathf.Atan2(movement.x, movement.y) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(new Vector3(0, angle, 0)), Time.deltaTime * rotationSpeed);
+        }
+
+        rigid.velocity = transform.forward * moveSpeed;
+    }
+
+    private void OnMoveUp()
+    {
+        Debug.Log("up");
+    }
+    private void OnMoveDown()
+    {
+        Debug.Log("down");
+    }
+}
